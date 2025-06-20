5170f66e4e3afe100f963e1dd251cf9df20561af
Samuel Musyoka <96131313+crebi6@users.noreply.github.com>
2025-06-20 12:31:53 +0300

    Update asyncWrapper.js

--- utils/asyncWrapper.js
+++ utils/asyncWrapper.js
@@ -1,1 +1,9 @@
+// utils/asyncWrapper.js - Wrapper for handling async errors
 
+const asyncWrapper = (fn) => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+module.exports = { asyncWrapper };
